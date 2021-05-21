fdc35429357e39280fac134cb20b573ad23cb5aa
jaikko <65661383+jaikko@users.noreply.github.com>
2021-05-21 13:38:49 +0200

    Update urls.py

--- my_api/urls.py
+++ my_api/urls.py
@@ -1,13 +1,7 @@
-
-from django.conf.urls import url
 from django.urls.conf import include
-from rest_framework_extensions.routers import NestedRouterMixin
-from .views import CommentsViewSet, IssuesViewSet, RegisterAPI,ProjectViewSet, ContributorViewSet, UserViewSet
+from .views import CommentsViewSet, IssuesViewSet, RegisterAPI, ProjectViewSet, ContributorViewSet
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 from rest_framework import routers
 from rest_framework_nested import routers
 
@@ -15,11 +9,11 @@ from rest_framework_nested import routers
 router = routers.DefaultRouter()
 router.register(r'projects', ProjectViewSet, basename="Project")
 
-project_router = routers.NestedSimpleRouter(router, r'projects', lookup= 'project')
+project_router = routers.NestedSimpleRouter(router, r'projects', lookup='project')
 project_router.register(r'users', ContributorViewSet, basename='project-users')
-issues_routeur = routers.NestedSimpleRouter(router, r'projects', lookup= 'project')
+issues_routeur = routers.NestedSimpleRouter(router, r'projects', lookup='project')
 issues_routeur.register(r'issues', IssuesViewSet, basename='project-issues')
-comments_routeur = routers.NestedSimpleRouter(issues_routeur,r'issues', lookup= 'issue')
+comments_routeur = routers.NestedSimpleRouter(issues_routeur, r'issues', lookup='issue')
 comments_routeur.register(r'comments', CommentsViewSet, basename='comment')
 
 
@@ -31,5 +25,4 @@ urlpatterns = [
     path('signup', RegisterAPI.as_view(), name='register'),
     path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-   
 ]
